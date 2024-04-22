9783bbe83e8b1916924087a362e56a71162a7d77
Alesandro Diogo <alesandromendesdiogo24@gmail.com>
2024-04-22 15:16:59 +0100

    Implementando o servico para CategoryService

---
+++ CleanArchMVC.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchMVC.Application.DTOs;
+
+namespace CleanArchMVC.Application.Interfaces;
+public interface ICategoryService
+{
+    Task<IEnumerable<CategoryDTO>> GetCategories();
+    Task<CategoryDTO> GetById(int? id);
+    Task Add(CategoryDTO categoryDto);
+    Task Update(CategoryDTO categoryDto);
+    Task Remove(int? id);
+}
---
+++ CleanArchMVC.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CleanArchMVC.Application.DTOs;
+using CleanArchMVC.Domain.Entities;
+
+namespace CleanArchMVC.Application.Mappings;
+public class DomainToDTOMappingProfile : Profile
+{
+    public DomainToDTOMappingProfile()
+    {
+        CreateMap<Category, CategoryDTO>().ReverseMap();
+        CreateMap<Product, ProductDTO>().ReverseMap();
+    }
+}
