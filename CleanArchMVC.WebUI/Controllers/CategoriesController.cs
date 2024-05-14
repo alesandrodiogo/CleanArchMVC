680a8d34c664235f09fbddeabcb2cdb5039bb5d5
Alesandro Diogo <alesandromendesdiogo24@gmail.com>
2024-05-14 15:39:21 +0100

    Implementando a acao delete category

--- CleanArchMVC.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMVC.WebUI/Controllers/CategoriesController.cs
@@ -65,4 +65,24 @@ public class CategoriesController : Controller
 
         return View(categoryDTO);
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) NotFound();
+
+        var categoryDto = await _categoryService.GetById(id);
+
+        if (categoryDto == null) return NotFound(); 
+        
+        return View(categoryDto);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _categoryService.Remove(id);
+
+        return RedirectToAction(nameof(Index));
+    }
 }
