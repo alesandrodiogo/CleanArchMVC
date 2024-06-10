16165577049732aa04195b0855fd6345bc22bae9
Alesandro Diogo <alesandromendesdiogo24@gmail.com>
2024-06-10 16:44:19 +0100

    Testando o ProductsController

--- CleanArchMVC.Application/DTOs/ProductDTO.cs
+++ CleanArchMVC.Application/DTOs/ProductDTO.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace CleanArchMVC.Application.DTOs;
 public class ProductDTO
@@ -36,6 +37,7 @@ public class ProductDTO
     [DisplayName("Product Image")]
     public string Image { get; set; }
 
+    [JsonIgnore]
     public Category? Category { get; set; } 
 
     [DisplayName("Categories")]
